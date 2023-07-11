f7ff7bcac41a16ef79239b5fd1cb74fbfc9d44ca
DISS <ecuevas@diss.com>
2023-07-11 14:41:00 -0400

    Add Date Support to GPT Messages

--- Sources/SwiftOpenAI/OpenAI/DataModels/Message/MessageChatGPT.swift
+++ Sources/SwiftOpenAI/OpenAI/DataModels/Message/MessageChatGPT.swift
@@ -4,10 +4,12 @@ public struct MessageChatGPT: Identifiable, Hashable {
     public var id: UUID
     public var text: String
     public var role: MessageRoleType
+    public var date: Date
 
-    public init(text: String, role: MessageRoleType) {
+    public init(text: String, role: MessageRoleType, date:Date = Date()) {
         self.id = UUID()
         self.text = text
         self.role = role
+        self.date = date
     }
 }
