628af974ba4a56305a8ee1728cb8ca86dc9d43ac
DISS <ecuevas@diss.com>
2024-08-07 14:15:27 -0400

    Fix

--- Sources/SwiftOpenAI/OpenAI/DataModels/Message/MessageChatGPT.swift
+++ Sources/SwiftOpenAI/OpenAI/DataModels/Message/MessageChatGPT.swift
@@ -4,10 +4,12 @@ public struct MessageChatGPT: Identifiable, Hashable {
     public var id: UUID
     public var text: String
     public var role: MessageRoleType
+    public var date: Date
 
-    public init(text: String, role: MessageRoleType) {
-        self.id = UUID()
+    public init(text: String, role: MessageRoleType, date: Date = Date(), id: UUID = UUID()) {
+        self.id = id
         self.text = text
         self.role = role
+        self.date = date
     }
 }
